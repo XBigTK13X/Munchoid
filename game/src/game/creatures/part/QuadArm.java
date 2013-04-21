3a23d2eba4abae4d86cc6dd014aea3e3f0db9115
XBigTK13X <xbigtk13x@gmail.com>
2013-04-21 15:01:50 -0400

    Arms now drawn and oriented with correct origin.

--- game/src/game/creatures/part/QuadArm.java
+++ game/src/game/creatures/part/QuadArm.java
@@ -8,13 +8,16 @@ public class QuadArm implements Design {
         if (armHeight == 0) {
             armHeight = 1;
         }
-        for (int ii = 0; ii < width;ii++) {
-            for(int jj=armHeight;jj< armHeight*2;jj++){
+        for (int ii = 0; ii < width; ii++) {
+            for (int jj = armHeight; jj < armHeight * 2; jj++) {
                 result[ii][jj] = true;
             }
         }
-        for (int ii = width-armHeight; ii < width;ii++) {
-            for(int jj= 0;jj<armHeight;jj++){
+
+        int handHeight = (int) (armHeight * 1.5);
+        handHeight = handHeight > height ? height : handHeight;
+        for (int ii = width - armHeight; ii < width; ii++) {
+            for (int jj = armHeight - armHeight / 2; jj < armHeight + handHeight; jj++) {
                 result[ii][jj] = true;
             }
         }
