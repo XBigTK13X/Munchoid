500c6e48b167dd47591c000e0b1d25fcbc6f0f7e
XBigTK13X <xbigtk13x@gmail.com>
2013-07-25 21:16:26 -0400

    Easier to run check_bots on older data.

--- script/check_bots.sh
+++ script/check_bots.sh
@@ -1,5 +1,6 @@
-grep -r -I Exception ./game/logs | wc -l | xargs -I % echo "% exceptions"
-grep -r -I WIN ./game/logs | wc -l | xargs -I % echo "% wins"
-grep -r -I LOSE ./game/logs | wc -l | xargs -I % echo "% losses"
-python script/stats.py ./game/logs/
+target=${1-"./game/logs"}
+grep -r -I Exception $target | wc -l | xargs -I % echo "% exceptions"
+grep -r -I WIN $target | wc -l | xargs -I % echo "% wins"
+grep -r -I LOSE $target | wc -l | xargs -I % echo "% losses"
+python script/stats.py $target
 ls game | grep hs_ | wc -l | xargs -I % echo "Java crashed % times. Probably because it ran out of memory."
