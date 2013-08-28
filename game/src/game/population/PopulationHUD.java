884f42ac580d5b0cd43094a328ebeeb32c081ae1
XBigTK13X <xbigtk13x@gmail.com>
2013-08-28 17:15:08 -0400

    Population spreads as towns over the region map.

--- game/src/game/population/PopulationHUD.java
+++ game/src/game/population/PopulationHUD.java
@@ -7,7 +7,6 @@ import game.creatures.style.Outline;
 import sps.core.Point2;
 import sps.display.Screen;
 import sps.display.Window;
-import sps.draw.Colors;
 import sps.draw.ProcTextures;
 import sps.draw.SpriteMaker;
 import sps.util.MathHelper;
@@ -16,63 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PopulationHUD {
-    private static final int __colorMin = 10;
-    private static final int __colorMax = 245;
-    //Elevation base (single color range in perlin noise)
-    private static final int __c = 255;
-
-
-    private static Color elevationToColor(int elevation) {
-        int r = 0;
-        int g = 0;
-        int b = 0;
-        if (elevation < __c / 2) {
-            if (elevation > __c / 3) {
-                b = __c - elevation;
-            }
-            else {
-                r = g = __c - elevation - 30;
-                b = __c - elevation / 2 + 30;
-            }
-        }
-        else {
-            if (elevation > 180) {
-                r = g = elevation;
-                b = elevation - 50;
-            }
-            else {
-                g = elevation;
-            }
-        }
-
-        return Colors.rgb(cap(r), cap(g), cap(b));
-    }
-
-    private static int cap(int colorComponent) {
-        return Math.min(Math.max(__colorMin, colorComponent), __colorMax);
-    }
-
-    private static Color[][] world(int width, int height) {
-        Color[][] base = ProcTextures.perlin(width, height, Colors.rgb(0, 255, 255), Colors.rgb(255, 255, 255), 9);
-
-        if (GameConfig.OptSmoothRegionMap) {
-            ProcTextures.blur(base, 2);
-        }
-
-        for (int ii = 0; ii < width; ii++) {
-            for (int jj = 0; jj < height; jj++) {
-                int elevation = (int) (255 * base[ii][jj].r);
-                base[ii][jj] = elevationToColor(elevation);
-            }
-        }
-
-
-        return base;
-    }
-
     private Population _population;
 
-    private Sprite _bg;
+    private WorldMap _map;
     private Sprite _popIcon;
     private int _maxIconsWide;
     private int _maxIconsHigh;
@@ -80,40 +25,44 @@ public class PopulationHUD {
     private List<Point2> _iconPositions;
     private int _pad = 1;
 
+    private Color _iconColor = new Color(Color.WHITE);
+
     public PopulationHUD(Population population, Point2 size, Point2 position) {
         _population = population;
 
-        Color[][] bgBase = PopulationHUD.world((int) size.X, (int) size.Y);
-        _bg = SpriteMaker.get().fromColors(bgBase);
-        _bg.setPosition(position.X, position.Y);
+        _map = new WorldMap((int) size.X, (int) size.Y, position);
 
-        Color[][] popIconBase = ProcTextures.monotone((int) Screen.width(1), (int) Screen.width(1), Color.WHITE);
-        Outline.single(popIconBase, Color.BLACK, 1);
+        Color[][] popIconBase = ProcTextures.monotone((int) Screen.width(1), (int) Screen.width(1), _iconColor);
+        Outline.single(popIconBase, Color.BLACK, 2);
         _popIcon = SpriteMaker.get().fromColors(popIconBase);
 
-        _maxIconsHigh = (int) (_bg.getHeight() / (_popIcon.getHeight() + _pad)) - 1;
-        _maxIconsWide = (int) (_bg.getWidth() / (_popIcon.getWidth() + _pad)) - 1;
+        _maxIconsHigh = (int) (size.Y / (_popIcon.getHeight() + _pad)) - 1;
+        _maxIconsWide = (int) (size.X / (_popIcon.getWidth() + _pad)) - 1;
         _maxIcons = _maxIconsHigh * _maxIconsWide;
+
+        _iconPositions = new ArrayList<>();
     }
 
     public void recalcIcons() {
-        _iconPositions = new ArrayList<>();
-
         int iconsToDraw = MathHelper.clamp((int) (_maxIcons * ((float) _population.getSize()) / GameConfig.PopulationMax), 1, _maxIcons);
 
-        int iconCount = 0;
-        for (int ii = 0; ii < _maxIconsHigh; ii++) {
-            for (int jj = 0; jj < _maxIconsWide && iconCount < iconsToDraw; jj++) {
-                iconCount++;
-                _iconPositions.add(new Point2(_bg.getX() + jj * _popIcon.getWidth() + _popIcon.getWidth() / 2 + _pad * jj, _bg.getY() + ii * _popIcon.getHeight() + _popIcon.getHeight() / 2 + _pad * ii));
-            }
+        while (iconsToDraw < _iconPositions.size()) {
+            Point2 kill = _iconPositions.get(_iconPositions.size() - 1);
+            _iconPositions.remove(_iconPositions.size() - 1);
+            _map.resetSpace(kill.add(-_map.getPosition().X, _map.getPosition().Y).add(_popIcon.getWidth() / 2, _popIcon.getHeight() / 2));
+        }
+
+        while (iconsToDraw > _iconPositions.size()) {
+            _iconPositions.add(_map.getOpenSpace().add(_map.getPosition()).add(-_popIcon.getWidth() / 2, -_popIcon.getHeight() / 2));
         }
     }
 
     public void draw() {
-        Window.get().draw(_bg);
+        _map.draw();
         for (Point2 p : _iconPositions) {
             _popIcon.setPosition(p.X, p.Y);
+            _iconColor.a = 1;
+            _popIcon.setColor(_iconColor);
             Window.get().draw(_popIcon);
         }
     }
---
+++ game/src/game/population/WorldMap.java
@@ -0,0 +1,146 @@
+package game.population;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import game.GameConfig;
+import sps.core.Point2;
+import sps.core.RNG;
+import sps.display.Window;
+import sps.draw.Colors;
+import sps.draw.ProcTextures;
+import sps.draw.SpriteMaker;
+
+public class WorldMap {
+    public static final int __c = 255;
+
+    private static enum Biome {
+        Land(true, __c / 2, 180),
+        Desert(true, 180, __c),
+        Water(false, __c / 3, __c / 2),
+        Ice(true, 0, __c / 3);
+
+        private static final int __colorMin = 10;
+        private static final int __colorMax = 245;
+
+        public final boolean Habitable;
+        public final int ElevationMax;
+        public final int ElevationMin;
+
+        private Biome(boolean habitable, int elevationMin, int elevationMax) {
+            Habitable = habitable;
+            ElevationMax = elevationMax;
+            ElevationMin = elevationMin;
+        }
+
+        public static Biome fromElevation(int elevation) {
+            for (Biome b : values()) {
+                if (b.ElevationMin <= elevation && b.ElevationMax >= elevation) {
+                    return b;
+                }
+            }
+            throw new RuntimeException("Undefined biome elevation!");
+        }
+
+        public static Color getColor(int elevation) {
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            switch (fromElevation(elevation)) {
+                case Land:
+                    g = elevation;
+                    break;
+                case Desert:
+                    r = g = elevation;
+                    b = elevation - 50;
+                    break;
+                case Water:
+                    b = __c - elevation;
+                    break;
+                case Ice:
+                    r = g = __c - elevation / 3 - 30;
+                    b = __c - elevation / 2 + 30;
+                    break;
+            }
+
+            return Colors.rgb(cap(r), cap(g), cap(b), 255);
+        }
+
+        private static int cap(int colorComponent) {
+            return Math.min(Math.max(__colorMin, colorComponent), __colorMax);
+        }
+    }
+
+    private Sprite _sprite;
+    private int[][] _habitableZones;
+    private Point2 _position;
+
+
+    private static final int __habitBuffer = 25;
+
+
+    public WorldMap(int width, int height, Point2 position) {
+        _position = position;
+        Color[][] base = ProcTextures.perlin(width, height, Colors.rgb(0, 255, 255), Colors.rgb(255, 255, 255), 9);
+        _habitableZones = new int[width][height];
+
+        if (GameConfig.OptSmoothRegionMap) {
+            ProcTextures.blur(base, 2);
+        }
+
+        for (int ii = 0; ii < width; ii++) {
+            for (int jj = 0; jj < height; jj++) {
+                int elevation = (int) (255 * base[ii][jj].r);
+                base[ii][jj] = Biome.getColor(elevation);
+                if (ii > __habitBuffer && ii < width - __habitBuffer && jj > __habitBuffer && jj < height - __habitBuffer) {
+                    _habitableZones[ii][jj] = Biome.fromElevation(elevation).Habitable ? 1 : 0;
+                }
+                else {
+                    _habitableZones[ii][jj] = 0;
+                }
+            }
+        }
+
+        _sprite = SpriteMaker.get().fromColors(base);
+        _sprite.setPosition(position.X, position.Y);
+    }
+
+    public void resetSpace(Point2 location) {
+        int x = (int) (location.X);
+        int y = (int) (location.Y);
+        _habitableZones[x][y] = 1;
+    }
+
+    private static final int requiredLivingSpace = 15;
+
+    public Point2 getOpenSpace() {
+        while (true) {
+            Point2 location = new Point2(RNG.next(0, _habitableZones.length), RNG.next(0, _habitableZones[0].length));
+            if (_habitableZones[(int) location.X][(int) location.Y] == 1) {
+                boolean success = true;
+                for (int ii = -requiredLivingSpace; ii < requiredLivingSpace; ii++) {
+                    for (int jj = -requiredLivingSpace; jj < requiredLivingSpace; jj++) {
+                        int x = (int) (ii + location.X);
+                        int y = (int) (jj + location.Y);
+                        if (x >= 0 && x < _habitableZones.length && y >= 0 && y < _habitableZones[0].length) {
+                            if (_habitableZones[x][y] != 1) {
+                                success = false;
+                            }
+                        }
+                    }
+                }
+                if (success) {
+                    _habitableZones[(int) location.X][(int) location.Y] = 2;
+                    return location;
+                }
+            }
+        }
+    }
+
+    public Point2 getPosition() {
+        return _position;
+    }
+
+    public void draw() {
+        Window.get().draw(_sprite);
+    }
+}
