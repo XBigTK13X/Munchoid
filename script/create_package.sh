6e3473fb9f1e18763fa58d4c011ac64980877ef3
XBigTK13X <xbigtk13x@gmail.com>
2013-09-25 07:13:22 -0400

    Remove launcher/game layout, since launcher is now handled by conyay.

--- script/create_package.sh
+++ script/create_package.sh
@@ -2,16 +2,16 @@
 
 echo "== Preparing a working directory for the new package: pkg"
 rm -rf pkg
-rm -rf game/assets/graphics/sprites/.directory
+rm -rf assets/graphics/sprites/.directory
 mkdir pkg
 
 echo "== Compiling the game and the launcher"
 mvn -q clean package
 
 echo "== Copying resources"
-cp -r game/assets ./pkg/assets
-cp -r dist/* ./pkg
-cp game/target/game-jar-with-dependencies.jar pkg/game.jar
+cp -r assets pkg/assets
+cp -r dist/* pkg
+cp target/game-jar-with-dependencies.jar pkg/game.jar
 
 git log --max-count=1 > pkg/assets/data/git.txt
 
