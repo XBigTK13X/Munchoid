6fe8d925bc417956d5f718dd195d931e0b74a111
XBigTK13X <xbigtk13x@gmail.com>
2013-11-21 16:14:45 -0600

    New logs location.

--- script/check_bots.sh
+++ script/check_bots.sh
@@ -1,6 +1,6 @@
-target=${1-"./game/logs"}
+target=${1-"logs/"}
 grep -r -I Exception $target | wc -l | xargs -I % echo "% exceptions"
 grep -r -I WIN $target | wc -l | xargs -I % echo "% wins"
 grep -r -I LOSE $target | wc -l | xargs -I % echo "% losses"
 python script/stats.py $target
-ls game | grep hs_ | wc -l | xargs -I % echo "Java crashed % times. Probably because it ran out of memory."
+ls game | grep hs_ | wc -l | xargs -I % echo "Java crashed % times. If it happened at then, it's probably because the JVM ran out of memory."
