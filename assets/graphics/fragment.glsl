5f957f4e08a8743f7f7f93ff7b5187a29b1fcad7
Tim Kretschmer <tkretschmer@radiantblue.com>
2013-11-29 16:42:42 -0600

    More robust b/c/s fragment shader manipulation. Brightness/Contrast still not working in shader.

--- assets/graphics/fragment.glsl
+++ assets/graphics/fragment.glsl
@@ -1,18 +1,18 @@
 #ifdef GL_ES
     precision mediump float;
 #endif
-
 varying vec4 v_color;
 varying vec2 v_texCoords;
+
 uniform sampler2D u_texture;
+
 uniform float u_brightness;
 uniform float u_contrast;
+uniform float u_saturation;
 
 void main() {
     vec4 color = v_color * texture2D(u_texture, v_texCoords);
-    color.rgb /= color.a;
-    color.rgb = ((color.rgb - 0.5f) * max(u_contrast,0)) + 0.5f;
-    color.rgb += u_brightness;
-    color.rgb *= color.a;
+	color = vec4(color.rgb + u_brightness,color.a);
     gl_FragColor = color;
+
 }
--- src/sps/display/render/ShaderBatch.java
+++ src/sps/display/render/ShaderBatch.java
@@ -5,20 +5,31 @@ import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 //Modified from: https://github.com/mattdesl/lwjgl-basics/wiki/LibGDX-Brightness-&-Contrast
 public class ShaderBatch extends SpriteBatch {
-    private Integer _brightnessLocation = null;
-    private Integer _contrastLocation = null;
-    private float _brightness = 0f;
-    private float _contrast = -1f;
+
+    private static final int GLSL_VALUE_NOT_PRESENT = -1;
+
+    private float _brightness = 1f;
+    private float _contrast = 1f;
+    private float _saturation = 0f;
+
     private float _lastBrightness = Float.MAX_VALUE;
     private float _lastContrast = Float.MAX_VALUE;
+    private float _lastSaturation = Float.MAX_VALUE;
+
+    private boolean _brightnessControlsSupported;
+    private boolean _contrastControlsSupported;
+    private boolean _saturationControlsSupported;
+
     private ShaderProgram _shaders;
 
     public ShaderBatch(ShaderProgram shaders) {
         _shaders = shaders;
-        _brightnessLocation = _shaders.getUniformLocation("u_brightness");
-        _contrastLocation = _shaders.getUniformLocation("u_contrast");
+        _brightnessControlsSupported = _shaders.getUniformLocation("u_brightness") != GLSL_VALUE_NOT_PRESENT;
         setBrightness(_brightness);
+        _contrastControlsSupported = _shaders.getUniformLocation("u_contrast") != GLSL_VALUE_NOT_PRESENT;
         setContrast(_contrast);
+        _saturationControlsSupported = _shaders.getUniformLocation("u_saturation") != GLSL_VALUE_NOT_PRESENT;
+        setSaturation(_saturation);
         setShader(_shaders);
     }
 
@@ -27,18 +38,32 @@ public class ShaderBatch extends SpriteBatch {
     }
 
     public void setBrightness(float brightness) {
-        _brightness = brightness;
-        if (_lastBrightness != _brightness) {
-            _shaders.setUniformf(_brightnessLocation, _brightness);
-            _lastBrightness = _brightness;
+        if (_brightnessControlsSupported) {
+            _brightness = brightness;
+            if (_lastBrightness != _brightness) {
+                _shaders.setUniformf("u_brightness", _brightness);
+                _lastBrightness = _brightness;
+            }
         }
     }
 
     public void setContrast(float contrast) {
-        _contrast = contrast;
-        if (_lastContrast != _contrast) {
-            _shaders.setUniformf(_contrastLocation, _contrast);
-            _lastContrast = _contrast;
+        if (_contrastControlsSupported) {
+            _contrast = contrast;
+            if (_lastContrast != _contrast) {
+                _shaders.setUniformf("u_contrast", _contrast);
+                _lastContrast = _contrast;
+            }
+        }
+    }
+
+    public void setSaturation(float saturation) {
+        if (_saturationControlsSupported) {
+            _saturation = saturation;
+            if (_lastSaturation != _saturation) {
+                _shaders.setUniformf("u_saturation", _saturation);
+                _lastSaturation = _saturation;
+            }
         }
     }
 }
