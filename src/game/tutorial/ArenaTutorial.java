0d0a9b8ee52b272e4d9b3a5b8501d80d3536ac42
XBigTK13X <xbigtk13x@gmail.com>
2014-01-03 10:25:03 -0600

    Typo

--- src/game/tutorial/ArenaTutorial.java
+++ src/game/tutorial/ArenaTutorial.java
@@ -5,7 +5,7 @@ import sps.bridge.Commands;
 public class ArenaTutorial extends Tutorial {
     public ArenaTutorial() {
         addStep("Use " + Commands.get("MoveUp") + "," + Commands.get("MoveRight") + "," + Commands.get("MoveDown") + ", and " + Commands.get("MoveLeft") + " to move.");
-        addStep("First you need to catch a creature, using " + Commands.get("Confirm"));
+        addStep("First you need to catch a creature, using " + Commands.get("Confirm") + ".");
         addStep("A creature turns blue if your pet can chomp it.\nIt turns red if your pet cannot.");
         addStep("The arrow over your head will always point to a chompable creature.");
         addStep("You only have a few moments before each fight to chomp.");
