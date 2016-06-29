d7ce9f899e2fa6b5051c32d024b85da9a0ddeb10
XBigTK13X <xbitk13x@gmail.com>
2016-06-29 10:13:32 -0500

    itch.io artifact upload

--- script/jenkins-build.sh
+++ script/jenkins-build.sh
@@ -5,3 +5,13 @@ cd $JENKINS_HOME/tools/conyay
 /bin/bash -c "./power.sh Munchoid $WORKSPACE/pkg"
 rm -rf $WORKSPACE/artifacts || true
 mv ./rap/Munchoid/packages $WORKSPACE/artifacts
+
+push(){
+	ZIP=$1
+	CHANNEL=$2
+	butler push $WORKSPACE/artifacts/$ZIP simplepathstudios/munchoid:$CHANNEL --userversion-file $WORKSPACE/assets/data/version.dat
+}
+
+push Munchoid-lin-64.zip linux-stable
+push Munchoid-mac.zip osx-stable
+push Munchoid-win-32.zip windows-stable
